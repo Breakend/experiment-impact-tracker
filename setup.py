bf8feba89a0dfc547d6468227e69207f4c5f6bdb
Peter Henderson <peter.henderson@mail.mcgill.ca>
2019-11-05 22:09:18 -0800

    Fix setup.py

---
+++ setup.py
@@ -0,0 +1,68 @@
+import sys
+import subprocess
+from setuptools import setup, find_packages
+from distutils.version import LooseVersion
+
+
+from distutils.util import convert_path
+
+main_ns = {}
+ver_path = convert_path('experiment_impact_tracker/version.py')
+with open(ver_path) as ver_file:
+    exec(ver_file.read(), main_ns)
+
+if sys.version_info.major != 3:
+    print('This Python is only compatible with Python 3, but you are running '
+          'Python {}. The installation will likely fail.'.format(sys.version_info.major))
+
+setup(name='experiment_impact_tracker',
+      packages= find_packages(),
+      include_package_data=True,
+      scripts=['scripts/create-compute-appendix','scripts/get-region-emissions-info', 'scripts/lookup-cloud-region-info'],
+      install_requires=[
+          'requests',
+          'bs4',
+          'shapely',
+          'scipy',
+          'joblib',
+          'numpy',
+          'pandas>0.25.0',
+          'matplotlib',
+          'py-cpuinfo',
+          'pylatex',
+          'ujson',
+          'geocoder',
+          'deepdiff',
+          'arrow',
+          'bootstrapped',
+          'jinja2',
+          'tqdm',
+          'psutil',
+          'seaborn'
+      ], 
+      extras_require={
+        'tests': [
+            'pytest==3.5.1',
+            'pytest-cov',
+            'pytest-env',
+            'pytest-xdist',
+        ],
+        'docs': [
+            'sphinx',
+            'sphinx-autobuild',
+            'sphinx-rtd-theme',
+            'recommonmark'
+        ]
+      },
+      description='A toolkit for easy evaluation of Deep RL algorithms.',
+      author='Peter Henderson',
+      url='https://github.com/Breakend/DeepRLEvaluationToolkit',
+      keywords="machine-learning ",
+      license="MIT",
+      version=main_ns['__version__']
+      )
+
+# python setup.py sdist
+# python setup.py bdist_wheel
+# twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+# twine upload dist/*
