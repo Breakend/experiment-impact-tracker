b119228985dadb4dcf4387625470ff8d38b08721
Peter Henderson <peter.henderson@mail.mcgill.ca>
2020-01-15 07:58:40 -0800

    Fixing possible bug on virtualenv packing

--- experiment_impact_tracker/py_environment/common.py
+++ experiment_impact_tracker/py_environment/common.py
@@ -1,4 +1,8 @@
 import pkg_resources
+import copyreg
+import zipimport
+
+copyreg.pickle(zipimport.zipimporter, lambda x: (x.__class__, (x.archive, )))
 
 def get_python_packages_and_versions(*args, **kwargs):
     return list(pkg_resources.working_set)
