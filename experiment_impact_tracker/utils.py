803d98a35e446f2024323f58b0b492a3069e0a82
Peter Henderson <peter.henderson@mail.mcgill.ca>
2021-01-09 17:34:15 -0800

    Update

--- experiment_impact_tracker/utils.py
+++ experiment_impact_tracker/utils.py
@@ -154,10 +154,15 @@ def gather_additional_info(info, logdir):
     )
 
     total_power_per_timestep = None
-    if has_gpu:
+    if has_gpu and kw_hr_rapl is not None:
         total_power_per_timestep = PUE * (kw_hr_nvidia + kw_hr_rapl)
     elif kw_hr_rapl is not None:
         total_power_per_timestep = PUE * (kw_hr_rapl)
+    elif has_gpu:
+        total_power_per_timestep = PUE * (kw_hr_nvidia)
+    else:
+        raise ValueError("Unable to get either GPU or CPU metric.")
+
 
     realtime_carbon = None
     total_power = (
